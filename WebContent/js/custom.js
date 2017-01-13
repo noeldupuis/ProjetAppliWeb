f77a661b83bbac62ff0ff136da3c7408781f31da
Noël Dupuis <noel.dupuis45370@gmail.com>
2017-01-13 12:05:13 +0100

    The problem with quotes found on the internet is that they are often not true. - Abraham Lincoln

--- WebContent/js/custom.js
+++ WebContent/js/custom.js
@@ -42,3 +42,29 @@ function setActive()
 {
   this.parentNode.setAttribute('class',this.perantNode.getAttribute('class')+' active');
 }
+
+$(document).ready(function() {
+  // navigation click actions 
+  $('.scroll-link').on('click', function(event){
+    event.preventDefault();
+    var sectionID = $(this).attr("data-id");
+    scrollToID('#' + sectionID, 750);
+  });
+});
+
+// scroll function
+function scrollToID(id, speed){
+  var offSet = 50;
+  var targetOffset = $(id).offset().top - offSet;
+  var mainNav = $('#main-nav');
+  $('html,body').animate({scrollTop:targetOffset}, speed);
+  if (mainNav.hasClass("open")) {
+    mainNav.css("height", "1px").removeClass("in").addClass("collapse");
+    mainNav.removeClass("open");
+  }
+}
+if (typeof console === "undefined") {
+    console = {
+        log: function() { }
+    };
+}
