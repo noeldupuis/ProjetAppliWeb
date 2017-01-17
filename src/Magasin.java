25f82bd06410f4859a03c3241db05979e90b25c8
florentdubois <duboisflorent8@gmail.com>
2017-01-17 01:33:46 +0100

    onToMany Magasin

--- src/Magasin.java
+++ src/Magasin.java
@@ -1,12 +1,11 @@
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.persistence.Column;
-import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 @Entity
@@ -19,7 +18,7 @@ public class Magasin {
 	
 	private String name;
 	
-	@ElementCollection
+	@OneToMany
 	private List<Rayon> rayons;	
 	
 	@OneToOne
