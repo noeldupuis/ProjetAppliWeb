21e1426c8e3475d9ac7a854f493ad1d4d32e85ff
omarbennani <omarbennani94@gmail.com>
2017-01-17 01:35:51 +0100

    maj stats

--- src/Stats.java
+++ src/Stats.java
@@ -46,14 +46,14 @@ public class Stats {
 	 * @param occurence
 	 */
 	public void majStats(Article article, int occurence) {
-		if (!statistics.containsKey(article)) {
-			ArticlePondere articleP= new ArticlePondere();
-			articleP.init(article, occurence);
-			statistics.add(articleP);
+		ArticlePondere ap = new ArticlePondere();
+		ap.init(article, occurence);
+		if (!statistics.contains(ap)) {
+			statistics.add(ap);
 		} else {
-			for (ArticlePondere ap : statistics) {
-				if (ap.getArticle == article) {
-					ap.setOccurence(ap.getOccurence() + occurence);
+			for (ArticlePondere arp : statistics) {
+				if (arp.getArticle() == article) {
+					arp.setOccurence(arp.getOccurence() + occurence);
 				}
 			}
 		}
@@ -63,94 +63,36 @@ public class Stats {
 	 * @param nb, le nombre voulu d'articles préférés
 	 * @return
 	 */
-	public List<Article> preferedArticles(int nb) {
+	public List<ArticlePondere> preferedArticles(int nb) {
 		// Trier la map
 		this.sortByValues();
 		
-		// Créer une liste d'articles
-		List<Article> list = new ArrayList<Article>();
-				
-		// Itérer sur la map pour récupérer les articles
-		Iterator<Entry<Article, Integer>> it = statistics.entrySet().iterator();
-		while (it.hasNext()) {
-			Entry<Article, Integer> pair = it.next();
-			list.add(pair.getKey());
-		}
-				
 		// Si nb est plus grand que le nombre d'articles déjà listés, retourner toute la liste
 		if (nb > statistics.size()) {			
-			return list;			
+			return statistics;			
 		// Sinon, retourner une sous-liste comprenant les nb articles préférés
 		} else {
-			return list.subList(0, nb);
+			return statistics.subList(0, nb);
 		}
 	}
 
-	/** Tri de la map statistics.
+	/** Tri de la liste statistics.
 	 * pas de paramètre, on travaille sur l'attribut statistics
 	 */
 	@SuppressWarnings({ "unchecked", "rawtypes" })
-	public void sortByValues() { 
-		List<?> list = new LinkedList(statistics.entrySet());
+	public void sortByValues() { 		
+		// Creation d'une LinkedList contenant statistics
+		List<ArticlePondere> sortedList = new LinkedList<ArticlePondere>();
+		sortedList.addAll(statistics);
+		
 		// Defined Custom Comparator here
-		Collections.sort(list, new Comparator() {
-			public int compare(Object o1, Object o2) {
-				return ((Comparable) ((Map.Entry) (o2)).getValue()).compareTo(((Map.Entry) (o1)).getValue());
+		Collections.sort(sortedList, new Comparator<ArticlePondere>() {
+			public int compare(ArticlePondere a1, ArticlePondere a2) {
+				return -((Comparable)a1.getOccurence()).compareTo(a2.getOccurence());
 	        }
 		});
 	
-		// Here I am copying the sorted list in HashMap
-		// using LinkedHashMap to preserve the insertion order
-		HashMap sortedHashMap = new LinkedHashMap<Article, Integer>();
-		for (Iterator it = list.iterator(); it.hasNext();) {
-			Map.Entry entry = (Map.Entry) it.next();
-			sortedHashMap.put(entry.getKey(), entry.getValue());
-		} 
-		statistics = sortedHashMap;
-	}
-	
-	/** Tri de la map statistics. /OBSOLETE/
-	 * pas de paramètre, on travaille sur l'attribut statistics
-	 * NB : statistics n'est en aucun cas modifié
-	 * @return une liste d'articles triés en fonction de leur occurrence
-	 */
-	public ArrayList<Article> sortMap() {		
-		// Liste à retourner
-		ArrayList<Article> list = new ArrayList<Article>();
-		// Entier pour comparer les occurences des articles
-		int n = 0;
-		// Copie de la map en attribut
-		Map<Article, Integer> copy = statistics;
-		// Itérateur sur la copie
-		Iterator<Entry<Article, Integer>> it1 = copy.entrySet().iterator();
-		
-		// Tant que la liste n'est pas complète
-		while (list.size() < statistics.size()) {			
-			// Parcours de la copie
-			while (it1.hasNext()) {
-				Entry<Article, Integer> pair = it1.next();
-				// Recherche de la plus grande occurrence
-				if (pair.getValue() > n) {
-					n = pair.getValue();
-				}			
-				it1.remove();
-			}
-			
-			// Récupération de l'article avec la plus grande occurrence
-			Article article = null;
-			for (Article tmp : copy.keySet()) {
-				if (copy.get(tmp) == n) {
-					article = tmp;
-					break;
-				}
-			}
-			list.add(article);
-			
-			// Suppression de l'article trouvé de la copie
-			copy.remove(article);	
-		}	
-		
-		return list;	
+		statistics = sortedList;
 	}
 	
 	/** Afficher les statistiques du client.
@@ -158,47 +100,9 @@ public class Stats {
 	 */
 	public void toString(Client client) {
 		System.out.println("Statistiques du client : " + client.getPrenom() + " " + client.getNom());
-		Iterator<Entry<Article, Integer>> it = statistics.entrySet().iterator();
-		while (it.hasNext()) {
-			Entry<Article, Integer> pair = it.next();
-			System.out.println("- Article " + pair.getKey().getName() + " commandé " + pair.getValue() + " fois");
+		for (ArticlePondere a : statistics) {
+			System.out.println("- Article " + a.getArticle().getName() + " commandé " + a.getOccurence() + " fois");
 		}
 	}
-	
-	/** Afficher les composants d'une liste.
-	 * @param list
-	 */
-	public static void listToString(List<Article> list) {
-		for (Article a : list) {
-			System.out.println(a.getName());
-		}
-	}
-	
-	/**public static void main(String[] args) {
-		Stats stats = new Stats();
-		
-		Article jambon = new Produit("jambon", null);
-		Article tomate = new Produit("tomate", null);
-		Article fromage = new Produit("fromage", null);
-		Article beurre = new Produit("beurre", null);
-		Article coca = new Produit("coca", null);
-		
-		stats.majStats(jambon, 1);
-		stats.majStats(tomate, 5);
-		stats.majStats(fromage, 3);
-		stats.majStats(beurre, 2);
-		stats.majStats(coca, 4);
-		
-		stats.sortByValues();
-		stats.toString(new Client("Bennani", "Omar", "Toulouse"));	
-		
-		List<Article> list1 = new ArrayList<Article>();
-		list1 = stats.preferedArticles(7);
-		listToString(list1);
-		
-		List<Article> list2 = new ArrayList<Article>();
-		list2 = stats.preferedArticles(3);
-		listToString(list2);
-	}*/
 
 }
